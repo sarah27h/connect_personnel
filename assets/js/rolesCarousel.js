091ed961367f42dc839d282a01611a39618bdfb9
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2021-01-07 00:48:08 +0200

    Feat: add owl carousel

--- assets/js/rolesCarousel.js
+++ assets/js/rolesCarousel.js
@@ -1,25 +1,21 @@
-$("#recipeCarousel").carousel({
-  interval: 10000,
-});
-
-$(".carousel .carousel-item").each(function () {
-  const minPerSlide = 3;
-  let next = $(this).next();
-  console.log(next);
-
-  if (!next.length) {
-    console.log(next);
-    next = $(this).siblings(":first");
-    console.log(next);
-  }
-  next.children(":first-child").clone().appendTo($(this));
-  // console.log(next.children(":first-child"));
-  for (let i = 0; i < minPerSlide; i++) {
-    next = next.next();
-    if (!next.length) {
-      next = $(this).siblings(":first");
-    }
-
-    next.children(":first-child").clone().appendTo($(this));
-  }
+// initialize Owl Carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+        dots: false,
+      },
+      990: {
+        items: 2,
+      },
+      1025: {
+        items: 3,
+      },
+    },
+  });
 });
